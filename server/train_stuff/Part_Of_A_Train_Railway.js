68483f39c9c1172059fd03f575334509f4e0ce44
Aeroel <hmm>
2024-11-19 11:13:41 +0300

    well, trying to make stuff related to train

---
+++ server/train_stuff/Part_Of_A_Train_Railway.js
@@ -0,0 +1,10 @@
+import { Entity } from "../Entity.js";
+
+export { Part_Of_A_Train_Railway }
+class Part_Of_A_Train_Railway extends Entity {
+    constructor() {
+        super();
+        this.addTag("Part_Of_A_Train_Railway");
+    }
+
+}
---
+++ server/train_stuff/Train.js
@@ -0,0 +1,19 @@
+import { Movable_Entity } from "../Movable_Entity.js"
+
+export { Train_Engine_Car }
+
+class Train_Car extends Movable_Entity {
+  // the train may temporarily move in the other direction to adjust alignment with station doors, but other than such exceptional cases, it will move in the specified direction, whether forward or backwards.
+  possibleMovements = ["a_direction", "the_opposite_direction"];
+  constructor() {
+    super();
+    this.setColor("brown");
+    this.addTag("Train");
+  }
+  setMovementDirective(direction) {
+    this.movementDirectionDirectiveOnRailway = direction;
+  }
+  behaviour() {
+
+  }
+}
