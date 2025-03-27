f9dfaa0c6d20c85b82613322f6a5b76165e48249
Aeroel <hmm>
2025-03-27 19:29:30 +0300

    Well, okay, everything seems to work, although the ts errors still remain. I have yet to see if using ts is worth it and the compiled js code is rather unsightly... not good

--- server/Helper_Functions.ts
+++ server/Helper_Functions.ts
@@ -1,4 +1,4 @@
-import { Socket_Functions } from "#root/Functions_To_Inject_Into_Socket_Instance.js";
+import { Socket_Functions } from "#root/Socket_Functions.js";
 import { SocketDataStorage } from "#root/SocketDataStorage.js";
 import { SocketStorage } from "#root/SocketStorage.js";
 import { World } from "#root/World.js";
@@ -8,26 +8,10 @@ import type { Socket } from "socket.io";
 export { Helper_Functions };
 class Helper_Functions {
 
-    static functionsToInjectIntoAndThenToRemoveFromSocket = [Socket_Functions.aMovementRequestHappenedJustNow, Socket_Functions.isMovementRequestFunctionalityOnCooldown];
     /* This does not do much conceptually. I store various pieces of data related to each socket in a separate container located inside SocketDataStorage. I also store the references to each socket inside SocketStorage (as opposed to using io methods to get sockets) mainly because I am not that familiar with io functions and it is kinda confusing using them. Using a separate SocketStorage container seems cleaner.
     // Same goes for the insertion of helper functions, I then use them like socket.helperFunction to do some operation, like to get last move request time, for example or to indicate that such a request just occurred and the value should be set to be now to be recalled later when checking the cooldown time
     */
 
-    static Apply_Mixins(targetClass, mixinClass) {
-        // Copy static properties and methods
-        Object.getOwnPropertyNames(mixinClass).forEach(name => {
-            if (name !== 'prototype' && name !== 'name') {
-                Object.defineProperty(targetClass, name, Object.getOwnPropertyDescriptor(mixinClass, name));
-            }
-        });
-
-        // Copy instance properties and methods
-        Object.getOwnPropertyNames(mixinClass.prototype).forEach(name => {
-            if (name !== 'constructor') {
-                Object.defineProperty(targetClass.prototype, name, Object.getOwnPropertyDescriptor(mixinClass.prototype, name));
-            }
-        });
-    }
     static isNumber(num: number) {
        return (typeof num === 'number' && isFinite(num));
     }
@@ -53,15 +37,4 @@ class Helper_Functions {
         const index = World.state.entities.indexOf(playerAssociatedWithSocket);
         World.state.entities.splice(index, 1);
     }
-    static removeTheInsertedFunctionsFromSocketInstance(socket) {
-        this.functionsToInjectIntoAndThenToRemoveFromSocket.forEach(func => {
-            delete socket[func.name];
-        });
-    }
-    static insertSomeHelperFunctionsIntoSocketInstance(socket) {
-        this.functionsToInjectIntoAndThenToRemoveFromSocket.forEach(func => {
-            socket[func.name] = func.bind(null, socket);
-        });
-    }
-
 }
