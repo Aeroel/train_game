253b34154b845290a11863dfbbccba3dfcef8a68
Aeroel <hmm>
2025-06-04 13:17:49 +0300

    dd

--- server/src/Helper_Functions.ts
+++ server/src/Helper_Functions.ts
@@ -14,6 +14,7 @@ class Helper_Functions {
     }
     static getLocalIP(): string {
         const nets = networkInterfaces();
+        console.log(nets);
         
         if(nets['Wi-Fi'] === undefined) {
             throw new Error('Undefined WIFI');
