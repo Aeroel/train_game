17e8b9567311fbbcb133b8ae5b59dafc4380493b
aeroel <aeroel>
2025-08-26 17:18:14 +0300

    serverside structure imps

--- server/src/Server_Stuff.ts
+++ server/src/Server_Stuff.ts
@@ -4,13 +4,15 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 
 import { Socket_Processor } from "#root/Socket_Processor.js"
+import { Helper_Functions } from "./Helper_Functions.js";
 
-export { Server_Creation }
 
-class Server_Creation {
+export { Server_Stuff }
+
+class Server_Stuff {
   static create() {
     
-   const { httpServer, io } = Server_Creation.getHttpServerAndIO();
+   const { httpServer, io } = this.getHttpServerAndIO();
    
   io.on("connection", (socket) => {
   Socket_Processor.onNewConnection(socket);
@@ -21,6 +23,20 @@ class Server_Creation {
     });
     
       return httpServer;
+  } 
+ static  createAndStartServer() {
+  const httpServer = this.create();
+ const arbitrary_number = 3000;
+ const port = arbitrary_number;
+ 
+      httpServer.listen(port);
+
+const LANIP = Helper_Functions.getLocalIP();
+const localhostIP = "127.0.0.1";
+console.log(`Yaaaay. Started a server on port ${port}.`);
+console.log(`LAN IP: ${LANIP}:${port}`);
+console.log(`localhost IP: ${localhostIP}:${port}.`);
+
   }
     static getHttpServerAndIO() {
         const app = express();
--- server/src/startThisToStartTheGameServer.ts
+++ server/src/startThisToStartTheGameServer.ts
@@ -1,23 +1,14 @@
-import { Add_Some_Entities_To_The_World } from "#root/Entities/Add_Some_Entities_To_The_World.js";
+import { Server_Stuff } from "#root/Server_Stuff.js";
+
 import { Game_Loop } from "#root/Game_Loop.js";
-import { Server_Creation } from "#root/Server_Creation.js";
-import { Helper_Functions } from "./Helper_Functions.js";
-import { setupREPLServers} from "#root/Console/REPL_Connection_Guide.js"
 
-const httpServer = Server_Creation.create();
-const arbitrary_number = 3000;
-const port = arbitrary_number;
-      httpServer.listen(port);
+import { Add_Some_Entities_To_The_World } from "#root/Entities/Add_Some_Entities_To_The_World.js";
 
-const LANIP = Helper_Functions.getLocalIP();
-const localhostIP = "127.0.0.1";
-console.log(`Yaaaay. Started a server on port ${port}.`);
-console.log(`LAN IP: ${LANIP}:${port}`);
-console.log(`localhost IP: ${localhostIP}:${port}.`);
+
+Server_Stuff.createAndStartServer();
+
   
 // add test entities
 Add_Some_Entities_To_The_World.doItNow();
   
      Game_Loop.theLoop();
-
-//setupREPLServers()
