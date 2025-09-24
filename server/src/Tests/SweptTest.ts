bfc0ac094c6c32282bcbbbc626d480aedd3acd6d
aeroel <aeroel>
2025-09-24 20:24:28 +0300

    huh, if I run swept abdtand second time swept badt (swapping a and b) I get different times. I wouldnexpect only the nornal to be reversed. This is an ereoe

--- server/src/Tests/SweptTest.ts
+++ server/src/Tests/SweptTest.ts
@@ -1,6 +1,7 @@
 import {
 mySweep
-} from "#root/Collision_Stuff/Collision_Checking_Methods/Sweep.js";
+} from "#root/Collision_Stuff/Collision_Checking_Methods/Sweep/Sweep.js";
+import { My_Assert} from "#root/My_Assert.js"
 import type {
   Direction,
   Position,
@@ -12,21 +13,32 @@ import type {
   Box
 } from "#root/Type_Stuff.js";
 
+export function SweptTest() {
 const dt = 50;
 const a = <Box_With_Velocity>{
   x: 0,
   y: 0,
-  width:10,
-  height:10,
+  width:8,
+  height:8,
   vx: 1,
   vy: 1
 }
 const b = <Box_With_Velocity>{
-  x: 20,
-  y: 20,
-  width:10,
-  height:10,
-  vx: 1,
-  vy: 1
+  x: 16,
+  y: 16,
+  width:16,
+  height:16,
+  vx: 0,
+  vy: 0
 }
-console.log(mySweep(a,b,dt))
+console.log("Swept equal time test if I swap a and b");
+const s1 =mySweep(a,b,dt)
+const s2 =mySweep(b,a,dt)
+My_Assert.that(s1 !== null && s2 !== null, "...")
+const time1= s1.time;
+const time2= s2.time;
+console.log(`Ab:${time1}`)
+console.log(`ba:${time2}`)
+My_Assert.that(time1===time2, "Swept a,b.time = swept b,a.time assertion failure")
+
+}
